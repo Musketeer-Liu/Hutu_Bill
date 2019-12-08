4482316caba07c57c949767108e702673a23c2c8
Yutong Liu <yutong.liu@uber.com>
2019-12-08 14:53:25 -0800

    Enhance Center Panel

--- src/gui/panel/SpendPanel.java
+++ src/gui/panel/SpendPanel.java
@@ -1,4 +1,25 @@
 package gui.panel;
 
+import javax.swing.*;
+
 public class SpendPanel {
+    public static SpendPanel instance = new SpendPanel();
+
+    JLabel lMonthSpend = new JLabel("本月消费");
+    JLabel lTodaySpend = new JLabel("今日消费");
+    JLabel lAvgSpendPerDay = new JLabel("日均消费");
+    JLabel lMonthLeft = new JLabel("本月剩余");
+    JLabel lDayAvgAvailable = new JLabel("日均可用");
+    JLabel lMonthLeftDay = new JLabel("距离月末");
+
+    JLabel vMonthSpend = new JLabel("￥2300");
+    JLabel vTodaySpend = new JLabel("￥25");
+    JLabel vAvgSpendPerDay = new JLabel("￥120");
+    JLabel vMonthLeft = new JLabel("￥2084");
+    JLabel vDayAvgAvailable = new JLabel("￥389");
+    JLabel vMonthLeftDay = new JLabel("15 天");
+
+    private SpendPanel() {
+
+    }
 }
--- src/util/CenterPanel.java
+++ src/util/CenterPanel.java
@@ -1,4 +1,62 @@
 package util;
 
-public class CenterPanel {
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * 由于 Swing没有提供可以居中的布局器，需要自己开发一个特别的类，继承自 JPanel
+ */
+public class CenterPanel extends JPanel {
+    private double rate;
+    private JComponent c;
+    private boolean strech;
+
+    public CenterPanel(double rate, boolean strech) {
+        this.setLayout(null);
+        this.rate = rate;
+        this.strech = strech;
+    }
+
+    public CenterPanel(double rate){
+        this(rate, true);
+    }
+
+    public void repaint() {
+        if (null != c) {
+            Dimension containerSize = this.getSize();
+            Dimension componentSize = c.getPreferredSize();
+
+            if (strech) {
+                c.setSize((int) (containerSize.width * rate), (int) (containerSize.height * rate));
+            } else {
+                c.setSize(componentSize);
+            }
+
+            c.setLocation(containerSize.width / 2 - c.getSize().width / 2, containerSize.height / 2 - c.getSize().height / 2 );
+        }
+        super.repaint();
+    }
+
+    public void show(JComponent p) {
+        this.c = p;
+        Component[] cs = getComponents();
+        for (Component c : cs) {
+            remove(c);
+        }
+        add(p);
+        this.updateUI();
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setSize(200, 200);
+        f.setLocationRelativeTo(null);
+        CenterPanel cp = new CenterPanel(0.85, true);
+        f.setContentPane(cp);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        JButton b = new JButton("abc");
+        cp.show(b);
+    }
 }
